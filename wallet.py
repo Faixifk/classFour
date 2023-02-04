ede4987d61c8fd1224eaba7a261f0eccac246cfc
Faixifk <35093954+Faixifk@users.noreply.github.com>
2023-02-04 15:12:28 +0500

    updated the code
    added wallet class

--- wallet.py
+++ wallet.py
@@ -1,1 +1,21 @@
 #Python wallet code
+
+#code
+
+class Wallet:
+
+    def __init__(self, balance):
+
+        self.balance = balance
+
+    def setAccount(self, balance):
+
+        self.balance = balance
+
+    def getAccount(self):
+
+        return self.balance
+
+    def removeAccount(self):
+
+        self.balance = 0
